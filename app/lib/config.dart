b53fe0cfd3b9ba216f2d6f91c3a5722b068b1214
Antonio Marcos <marcosmacedo784@gmail.com>
2025-12-16 21:04:06 -0300

    chore: Remove configurações Docker e atualiza URL da API para produção

--- app/lib/config.dart
+++ app/lib/config.dart
@@ -1,4 +1,4 @@
 // const String baseUrl = "https://endpointsmartsafe.online";
-const String baseUrl = "http://192.168.18.147:3002";
+const String baseUrl = "https://api.nuvemtec.shop";
 
 const String apiBaseUrl = "$baseUrl/api";
